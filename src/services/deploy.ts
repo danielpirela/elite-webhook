36fd781f3e984377fc0bf4b9ca72258acb1b6c4f
danielpirela <danielapirelac@gmail.com>
2025-05-07 18:02:29 +0000

    fix: path of deploy script

--- src/services/deploy.ts
+++ src/services/deploy.ts
@@ -1,15 +1,16 @@
-import { exec } from 'child_process';
+import { exec } from 'node:child_process'
+const path: string = '/home/prueba/scripts/deploy.sh'
 
-export function runDeployScript(scriptPath: string = '/home/prueba/dev/deploy.sh'): Promise<string> {
+export function runDeployScript(scriptPath: string = path): Promise<string> {
   return new Promise((resolve, reject) => {
     exec(`bash ${scriptPath}`, (error, stdout, stderr) => {
       if (error) {
-        console.error('❌ Error al ejecutar el script:', error);
-        reject(error);
+        console.error('❌ Error al ejecutar el script:', error)
+        reject(error)
       } else {
-        console.log('✅ Script ejecutado con éxito.');
-        resolve(stdout);
+        console.log('✅ Script ejecutado con éxito.')
+        resolve(stdout)
       }
-    });
-  });
+    })
+  })
 }
